128dafd3e3ef70807a27dcd22e53f2c6e0ce4c69
Claudio Manesso_fisso <clod182@hotmail.com>
2017-10-06 15:47:20 +0200

    css aggiunto alla schermata home

--- scambio appunti/home.php
+++ scambio appunti/home.php
@@ -3,19 +3,31 @@
   controllo_accesso();?>
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>Sito per lo scambio di appunti</title>
-</head>
-<body>
-  <h1>Scambio appunti ---HOMEPAGE---</h1>
-  Benvenuto <b><?php echo $_SESSION['nome_utente']?></b>!
-  Queste sono le operazioni che puoi fare:</h1>
-  <ul>
-    <li> <a href="Inserisci_dati_corso.php">Inserisci dati in un corso</a> </li>
-    <li> <a href="Inserisci_appunti_lezione_lc.php">Inserisci appunti per una lezione di un corso</a> </li>
-    <li> <a href="elenco_corsi.php">Visualizza elenco corsi</a> </li>
-    <li> <a href="ricerca_appunti.php">Ricerca appunti</a> </li>
-  </ul>
-</body>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+      <title>Sito per lo scambio di appunti</title>
+      <!-- Google Fonts-->
+      <link href="https://fonts.googleapis.com/css?family=Lobster|Quicksand" rel="stylesheet">
+      <!-- CSS-->
+      <link href="style.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+      <<div class="container">
+          <h1>-*-SCAMBIO APPUNTI-*-</h1>
+          Benvenuto <b><?php echo $_SESSION['nome_utente']?>!</b>
+          Queste sono le operazioni che puoi fare:</h1>
+          <ul>
+            <li> <a href="Inserisci_dati_corso.php" class="btn">Inserisci dati in un corso</a> </li>
+            <br />
+            <br />
+            <li> <a href="Inserisci_appunti_lezione_lc.php" class="btn">Inserisci appunti per una lezione di un corso</a> </li>
+            <br />
+            <br />
+            <li> <a href="elenco_corsi.php" class="btn">Visualizza elenco corsi</a> </li>
+            <br />
+            <br />
+            <li> <a href="ricerca_appunti.php" class="btn">Ricerca appunti</a> </li>
+          </ul>
+      </div>      
+    </body>
 </html>
